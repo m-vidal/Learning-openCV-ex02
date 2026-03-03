815da14d24b4398156321488f855566b5edcd649
m-vidal <vidal001eme@gmail.com>
2026-03-03 01:09:55 +0000

    mudando o nome de /imgs para assets

--- ex02.py
+++ ex02.py
@@ -5,11 +5,11 @@ from PIL import Image
 
 @st.cache_resource
 def load_model():
-    net = cv2.dnn.readNetFromCaffe(
-        "models/deploy.prototxt",
-        "models/res10_300x300_ssd_iter_140000.caffemodel"
-    )
-    return (net)
+	net = cv2.dnn.readNetFromCaffe(
+		"models/deploy.prototxt",
+		"models/res10_300x300_ssd_iter_140000.caffemodel"
+	)
+	return (net)
 
 net = load_model()
 
@@ -18,37 +18,36 @@ st.write("ResNet Model + SSD — OpenCV v3.3")
 in_frame = st.camera_input("Camera")
 
 if in_frame is not None:
-    image_pil = Image.open(in_frame)
-    frame = cv2.cvtColor(np.array(image_pil), cv2.COLOR_RGB2BGR)
+	image_pil = Image.open(in_frame)
+	frame = cv2.cvtColor(np.array(image_pil), cv2.COLOR_RGB2BGR)
 
-    height, width = frame.shape[:2]
+	height, width = frame.shape[:2]
 
-    blob = cv2.dnn.blobFromImage(
-        frame,
-        scalefactor=1.0,
-        size=(300, 300),
-        mean=(104.0, 177.0, 123.0),
-        swapRB=False
-    )
+	blob = cv2.dnn.blobFromImage(
+		frame,
+		scalefactor=1.0,
+		size=(300, 300),
+		mean=(104.0, 177.0, 123.0),
+		swapRB=False
+	)
 
-    net.setInput(blob)
-    detections = net.forward()
+	net.setInput(blob)
+	detections = net.forward()
 
-    frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    for i in range(detections.shape[2]):
-        trust = detections[0, 0, i, 2]
+	frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+	for i in range(detections.shape[2]):
+		trust = detections[0, 0, i, 2]
 
-        if trust < 0.5:
-            continue
-        
-        box = detections[0, 0, i, 3:7] * np.array([width, height, width, height])
-        x1, y1, x2, y2 = box.astype("int")
-        cv2.rectangle(frame_rgb, (x1, y1), (x2, y2), (0, 255, 0), 2)
+		if trust < 0.5:
+			continue
 
-        aux = Image.open("imgs/em0.png")
+		box = detections[0, 0, i, 3:7] * np.array([width, height, width, height])
+		x1, y1, x2, y2 = box.astype("int")
+		cv2.rectangle(frame_rgb, (x1, y1), (x2, y2), (0, 255, 0), 2)
 
-    roi = frame_rgb[y1:y2, x1:x2]
-    emoji_in_roi = cv2.GaussianBlur(roi, (99, 99), 30)
-    frame_rgb[y1:y2, x1:x2] = emoji_in_roi
-    st.image(aux, caption="Result", width=700)
+		aux = Image.open("assets/em0.png")
+
+	roi = frame_rgb[y1:y2, x1:x2]
+	frame_rgb[y1:y2, x1:x2] = aux
+	st.image(frame_rgb, caption="Result", width=700)
 
